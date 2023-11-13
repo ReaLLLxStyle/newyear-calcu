969beaed57df6519faf79c7409652e589983cb34
ReaLLLxStyle <145691510+ReaLLLxStyle@users.noreply.github.com>
2023-11-13 15:27:04 +0800

    Add files via upload

---
+++ newnewnewnew-main/calculator-super-latest-main/calculator-new-label-main/calculator-latest-main/calculator-latest-main/Calculator-main/Villasan/src/main/java/com/mycompany/villasan/Villasan.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.villasan;
+
+/**
+ *
+ * @author JCTECH
+ */
+public class Villasan {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
